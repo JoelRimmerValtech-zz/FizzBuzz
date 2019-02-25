98d9d611dc12345e3c7824d091a2e58322e2c929
Joel.Rimmer <joel.rimmer@JoelRimmers-MacBook-Pro.local>
2019-02-25 12:36:56 +0000

    up to Fezz

--- src/joel/fizzbuzz/Main.java
+++ src/joel/fizzbuzz/Main.java
@@ -5,17 +5,40 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         for (int number = 1; number < 101; number++) {
-
-            if(number %3 == 0 && number %5 == 0){
-                System.out.println("Fizzbuzz");}
-
-            else if(number %3 ==0){
-                System.out.println("Fizz");}
-
-            else if(number %5 ==0){
-                System.out.println("Buzz");}
-
-            else{System.out.println(number);}
+            doFizzbuzz(number);
         }
+        for (int number = 101; number < 201; number++) {
+            doFizzbuzz(number);
+        }
+    }
+
+    private static void doFizzbuzz(int number) {
+        if(number %11 ==0){
+            System.out.println("Bong");}
+
+        else if(number %3 == 0 && number %5 == 0 && number %7 ==0){
+            System.out.println("FizzBuzzBang");}
+
+        else if(number %3 == 0 && number %7 == 0){
+            System.out.println("FizzBang");}
+
+        else if(number %5 == 0 && number %7 == 0){
+            System.out.println("BuzzBang");}
+
+        else if(number %3 == 0 && number %5 == 0){
+            System.out.println("FizzBuzz");}
+
+        else if(number %3 ==0){
+            System.out.println("Fizz");}
+
+        else if(number %5 ==0){
+            System.out.println("Buzz");}
+
+        else if(number %7 ==0){
+            System.out.println("Bang");}
+
+
+
+        else{System.out.println(number);}
     }
 }

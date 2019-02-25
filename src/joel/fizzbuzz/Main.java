56f8ff6d392674e1f65339140e9a19cdd31ae2fc
Joel.Rimmer <joel.rimmer@JoelRimmers-MacBook-Pro.local>
2019-02-25 16:43:16 +0000

    FizzBuzzBang Part 2 complete

--- src/joel/fizzbuzz/Main.java
+++ src/joel/fizzbuzz/Main.java
@@ -1,5 +1,9 @@
 package joel.fizzbuzz;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,35 +14,57 @@ public class Main {
         for (int number = 101; number < 201; number++) {
             doFizzbuzz(number);
         }
+        for (int number = 201; number < 301; number++) {
+            doFizzbuzz(number);
+        }
     }
 
     private static void doFizzbuzz(int number) {
+
+        boolean Fizz=false;
+        boolean Buzz=false;
+        boolean Bang=false;
+        boolean Bong=false;
+        boolean Fezz=false;
+        boolean Reverse=false;
+
+        if(number %3 ==0){
+            Fizz=true;}
+        if(number %5 ==0){
+            Buzz=true;}
+        if(number %7 ==0){
+            Bang=true;}
         if(number %11 ==0){
-            System.out.println("Bong");}
+            Bong=true;}
+        if(number %13 ==0){
+            Fezz=true;}
+        if(number %17 ==0){
+            Reverse=true;}
 
-        else if(number %3 == 0 && number %5 == 0 && number %7 ==0){
-            System.out.println("FizzBuzzBang");}
+        List<String> fb = new ArrayList<>();
+        if (Fizz) {
+            fb.add("Fizz");}
+        if (Fezz) {
+            fb.add("Fezz");}
+        if (Buzz) {
+            fb.add("Buzz");}
+        if (Bang) {
+            fb.add("Bang");}
+        if (Bong) {
+            fb.clear();
+            if (Fezz) {
+                fb.add("Fezz");}
+            fb.add("Bong");}
+        if (Reverse) {
+            Collections.reverse(fb);}
 
-        else if(number %3 == 0 && number %7 == 0){
-            System.out.println("FizzBang");}
-
-        else if(number %5 == 0 && number %7 == 0){
-            System.out.println("BuzzBang");}
-
-        else if(number %3 == 0 && number %5 == 0){
-            System.out.println("FizzBuzz");}
-
-        else if(number %3 ==0){
-            System.out.println("Fizz");}
-
-        else if(number %5 ==0){
-            System.out.println("Buzz");}
-
-        else if(number %7 ==0){
-            System.out.println("Bang");}
-
-
-
-        else{System.out.println(number);}
+        StringBuilder FizzBuzz = new StringBuilder();
+        for (int listnumber = 0; listnumber <= fb.size() - 1; listnumber++) {
+            FizzBuzz.append(fb.get(listnumber));
+        }
+        if (FizzBuzz.length()>0) {
+            System.out.println(FizzBuzz);}
+        else {
+            System.out.println(number);}
     }
 }
---
+++ src/joel/fizzbuzz/NewMain.java
@@ -0,0 +1,8 @@
+package joel.fizzbuzz;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+
+    }
+}

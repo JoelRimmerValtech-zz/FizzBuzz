054585fbf6b7100d2f1e9444c68cace2b60b29c3
Joel.Rimmer <joel.rimmer@JoelRimmers-MacBook-Pro.local>
2019-02-25 11:55:06 +0000

    FizzBuzz working file

--- src/joel/fizzbuzz/Main.java
+++ src/joel/fizzbuzz/Main.java
@@ -3,6 +3,19 @@ package joel.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        for (int number = 1; number < 101; number++) {
+
+            if(number %3 == 0 && number %5 == 0){
+                System.out.println("Fizzbuzz");}
+
+            else if(number %3 ==0){
+                System.out.println("Fizz");}
+
+            else if(number %5 ==0){
+                System.out.println("Buzz");}
+
+            else{System.out.println(number);}
+        }
     }
 }
